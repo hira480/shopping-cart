6db167bd3d86c18bac847769692530cfef7abb58
hira480 <hiramannan8@gmail.com>
2022-02-13 01:53:57 +0600

    phone and case added

---
+++ app.js
@@ -0,0 +1,29 @@
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
+    let productNumber = productInput.value;
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // update case total
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+}
+// phone increase decrease event
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false);
+})
+
+// handel case increase decrease event
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', 59, true);
+})
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', 59, false);
+})
